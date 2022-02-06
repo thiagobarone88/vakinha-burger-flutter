a0fd4e4d1a4d57cb9d84295e2b3ae4354da0e288
thiagobarone88 <35409824+thiagobarone88@users.noreply.github.com>
2022-02-05 23:11:08 -0300

    atualizando

---
+++ lib/app/repositories/order/order_repository_impl.dart
@@ -0,0 +1,38 @@
+import 'package:vakinha_burger_mobile/app/core/rest_client/rest_client.dart';
+import 'package:vakinha_burger_mobile/app/models/order.dart';
+import 'package:vakinha_burger_mobile/app/models/order_pix.dart';
+import 'dart:developer';
+import 'order_repository.dart';
+
+class OrderRepositoryImpl implements OrderRepository {
+  final RestClient _restClient;
+
+  OrderRepositoryImpl({required RestClient restClient})
+      : _restClient = restClient;
+
+  @override
+  Future<OrderPix> createOrder(Order order) async {
+    final result = await _restClient.post('/order/', {
+      'userId': order.userId,
+      'cpf': order.cpf,
+      'address': order.address,
+      'items': order.items
+          .map((shoppingCard) => {
+                'quantity': shoppingCard.quantity,
+                'productId': shoppingCard.product.id,
+              })
+          .toList()
+    });
+
+    if (result.hasError) {
+      log(
+        'Erro ao realizar o pedido ${result.statusCode}',
+        error: result.statusText,
+        stackTrace: StackTrace.current,
+      );
+      throw RestClientException('Erro ao realizar o pedido');
+    }
+
+    return OrderPix.fromMap(result.body);
+  }
+}
